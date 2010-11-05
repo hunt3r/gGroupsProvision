dc7de70210f4472066504a874846e36fe82dcd24
Chris Hunter <hunter.christopher@gmail.com>
2010-11-05 19:50:13 -0400

    Added to GitHub

---
+++ gsettings.py
@@ -0,0 +1,103 @@
+'''This file is part of gGroupsProv.
+gGroupsProv is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gGroupsProv is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gGroupsProv.  If not, see <http://www.gnu.org/licenses/>.
+
+gGroupsProv was originally written by Chris Hunter <hunter.christopher@gmail.com> 
+'''
+
+
+from xml.etree import ElementTree as ET
+import os
+
+#Get the xml file
+xmlFile = os.path.abspath(__file__)
+xmlFile = os.path.dirname(xmlFile)
+#change this if you want to change the default xml filename
+xmlFile += "/settings.xml"
+
+#Parse the XML file into a an ElementTree
+try:
+    tree = ET.parse(xmlFile)
+except Exception, inst:
+    print "Error opening XML file %s: %s" % (xmlFile, inst)
+
+# returnNodeText
+# Utility method to return the text value of an xml node or a default if not found
+# Returns String
+def returnNodeText(node, default):
+    try:
+        if node != None:
+            return node.text
+        else:
+            return default
+    except Exception, inst:
+        print "Error with XML node %s in returnNodeText(): %s" % (node, inst)
+
+# getDbSettings
+# This parses the root ElementTree into a hashtable of settings values
+# for the MySQL database
+# Return Hashtable
+def getDbSettings():
+    #traverse the xml file (albeit simplistically)
+    root = tree.getroot()
+    settings = root.find('settings')
+    database = settings.find('database')
+    db_user=returnNodeText(database.find('db_user'), 'DefaultUser')
+    db_pass=returnNodeText(database.find('db_pass'),'DefaultPass')
+    db_host=returnNodeText(database.find('db_host'),'DefaultHost')
+    db=returnNodeText(database.find('db'),'DefaultDb')
+    db_port=returnNodeText(database.find('db_port'),3306)
+    settingsHash= {'db_user':db_user,'db_pass':db_pass,'db_host':db_host,'db':db,'db_port':db_port}
+    return settingsHash
+
+# getGoogleSettings()
+# Same as getDbSettings, only gets google apps info from XML file
+# Return Hashtable
+def getGoogleSettings():
+    #tree = ET.parse(xmlFile)
+    root = tree.getroot()
+    settings = root.find('settings')
+    google = settings.find('googleapps')
+    g_user=returnNodeText(google.find('g_user'), 'DefaultUser')
+    g_pass=returnNodeText(google.find('g_pass'),'DefaultPass')
+    g_domain=returnNodeText(google.find('g_domain'),'DefaultDomain')
+    settingsHash= {'g_user':g_user,'g_pass':g_pass,'g_domain':g_domain,'g_email':"%s@%s"%(g_user,g_domain)}
+    return settingsHash
+
+# cleanString()
+# simple utility method to remove illegal characters from a list ID
+def cleanString(str):
+    illegals=[' ', '%', '@', '$', '^', '"', "'",'&','*']
+    for i in illegals:
+        str=str.replace(i, "-")
+    return str
+
+# getGroups()
+# get a list of hashtables of all group data needed to create a google group
+# from a mysql datasource.
+def getGroups():
+    grpList=[]
+    root = tree.getroot()
+    groups = root.find('groups')
+    for group in  groups.findall('group'):
+        id=group.find('id')
+        perms=group.find('permissions')
+        name=group.find('name')
+        desc=group.find('description')
+        owner=group.find('owner')
+        query=group.find('query')
+        groupHash={'id':cleanString(id.text),'permissions':perms.text,'name':name.text,'description':desc.text,'owner':owner.text,'query':query.text}
+        grpList.append(groupHash)
+    return grpList
+
+
---
+++ provision.py
@@ -0,0 +1,221 @@
+'''This file is part of gGroupsProv.
+gGroupsProv is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gGroupsProv is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gGroupsProv.  If not, see <http://www.gnu.org/licenses/>.
+
+gGroupsProv was originally written by Chris Hunter <hunter.christopher@gmail.com> 
+'''
+
+
+import MySQLdb
+import gdata.apps.groups.service
+import re
+import urllib
+from xml.dom import minidom
+import gsettings as gs
+import time
+import sys
+
+#Begin Local Vars
+gsettings=gs.getGoogleSettings()
+dbsettings=gs.getDbSettings()
+groups=gs.getGroups()
+#Instantiate Google Apps service here                                                                                                                               
+service=gdata.apps.groups.service.GroupsService(email=gsettings['g_email'], domain=gsettings['g_domain'], password=gsettings['g_pass'])
+service.ProgrammaticLogin()
+#End Local Vars
+
+# validateEmail(string email)
+# This is a utility to be sure we're dealing with a valid email address
+def validateEmail(email):
+    if len(email) > 6:
+        if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
+            return 1
+        return 0
+
+# compareStr(String str1, String str2)
+# Simple string comparison
+def compareStr(str1, str2):
+    if str1 == str2:
+        return 1
+    else:
+        return 0
+
+# find(String str, List seq)
+# Simple utility to see if a string exists in a List)
+def find(str, seq):
+    for item in seq:
+        if compareStr(str,item): 
+            return item
+
+# groupExists(String groupId)
+# See if a group exists in this google apps container
+def groupExists(groupId):
+    groupFound=0
+    for group in service.RetrieveAllGroups():
+        if compareStr(groupId.lower(), str(group['groupId']).lower()):
+            groupFound=1
+            print "Found group: %s"%(groupId)
+    return groupFound
+
+# purgeGroup(String groupId)
+# If a group exists, delete it
+def purgeGroup(groupId): 
+    if groupExists(groupId):
+        try:
+            service.DeleteGroup(groupId)
+            print "found and deleted: %s"%( group['groupId'])
+        except Exception, err:
+            print err
+
+# addUserToGroup(String groupId, string email)
+# Adds an email address to the specific group
+def addUserToGroup(groupId, email):
+    try:
+        service.AddMemberToGroup(email, groupId) 
+        print "Adding %s to %s"%(email,groupId)
+        return 1
+    except Exception, err:
+        print err
+        return 0
+
+# removeUserFromGroup(String groupId, string email)
+# Removes an email from a giveng group
+def removeUserFromGroup(groupId, email):
+    try:
+        service.RemoveMemberFromGroup(email, groupId)
+        print "Removing %s from %s"%(email,groupId)
+        return 1
+    except Exception, err:
+        print err
+        return 0
+
+# convertGroupMembersToDict(MemberFeed members)
+# I had some trouble with doing lookups on this object, so I just made a quick dirty
+# conversion to a standard List, and did it that way (probably a better way, please comment)
+def convertGroupMembersToDict(members):
+    memberDict=[]
+    for member in members:
+        memberDict.append(member['memberId'])
+    return memberDict
+
+# getMembers(string groupId)
+# A wrapper to add error handling to this call
+def getMembers(groupId):
+    try:
+        return service.RetrieveAllMembers(groupId)
+    except Exception, err:
+        return err
+
+# syncGroup(String groupId, List dbUsers)
+# Compares what's found out on the cloud, with what we find in the database query
+# If something is found in the cloud and not in the query, it's deleted
+# If something is found in the database and not in the cloud, its added
+def syncGroup(groupId, dbUsers):
+    print "Syncing group %s"%(groupId)
+    if groupExists("%s@%s"%(groupId, gsettings['g_domain'])):
+        members=getMembers(groupId)
+        #Add new users to the group
+        print "Remove users not found in list"
+        for member in members:
+            if not find(member['memberId'].lower(), dbUsers):            
+              removeUserFromGroup(groupId, member['memberId'])
+        print "Add users found in database, but not already in google group"
+        #Remove users from the list that aren't in the datafeed anymore
+        for email in dbUsers:
+            #Probably a better way to search in the members collection here, kinda hacked it
+            if not find(email, convertGroupMembersToDict(members)):
+              addUserToGroup(groupId, email)
+            
+
+# addOwner(String grupId, String owner)
+# makes a call to service to add an owner to the group
+def addOwner(groupId, owner):
+    try:
+        #first we need to be sure this user is in the list
+        addUserToGroup(groupId, owner)
+        #sleep a second to be sure it's registered
+        time.sleep(1)
+        #Then we add them as an owner
+        service.AddOwnerToGroup(owner, groupId)
+        print "Adding %s as owner"%(owner)
+    except Exception, err:
+        print err
+
+# addOwners(Dictionary group)
+# Split the owner property, loop through list, add owners
+def addOwners(group):
+    for owner in group['owner'].split(','):
+        if validateEmail(owner):
+            addOwner(group['id'], owner)
+
+# getListOfEmailsFromDb(Dictionary group)
+# Creates a String List of email addresses from a database call defined in settings.xml          
+def getListOfEmailsFromDB(group):
+    #instantiate this dictionary
+    emails=[]
+    try:
+        #instantiate connectiont ot MySQL
+        db = MySQLdb.connect(host=dbsettings['db_host'], port=int(dbsettings['db_port']), user=dbsettings['db_user'], passwd=dbsettings['db_pass'], db=dbsettings['db'])
+        cursor = db.cursor()
+        #execute the query from the xml file
+        cursor.execute(group['query'])
+        #get emails from db query
+        for row in cursor.fetchall():
+        #if this is a valid email address, add it here
+            if validateEmail(row[0]):
+                emails.append(row[0].lower())
+    except Exception, err:
+        print err
+        #return empty or partial set
+        return emails
+    finally:
+        #clean up database connection
+        db.close()
+        #show some output
+        print "Number of emails captured for %s: %d" %(group['id'], len(emails)) 
+        return emails
+    
+# createGroup(Dictionary group)
+# This checks to see if a group exists, then if not, creates it based on the group hash passed
+def createGroup(group):
+    try:
+        if not groupExists("%s@%s"%(group['id'],gsettings['g_domain'])):
+            print "Provisioning Group: %s"%(group['id'])
+            service.CreateGroup(group['id'],"[%s] %s" % (group['permissions'],group['name']),group['description'], group['permissions'])    
+            time.sleep(3) #need to sleep the thread for a few seconds so group becomes recognized
+    except Exception,err:
+        print err
+
+# Main()
+def main():
+    try:
+        for group in groups:
+            try:
+                print "----------------------------------"
+                print "Starting %s "%(group['id'])
+                print "----------------------------------"
+                #Create the group if needed
+                createGroup(group)
+                #Sync emails existing and not existing
+                syncGroup(group['id'], getListOfEmailsFromDB(group))
+                #Add owners to the group
+                addOwners(group)
+            except Exception, err:
+                print err
+    except Exception, outer_err:
+        print outer_err
+        print "This is likely caused by an error in your settings.xml"
+
+#Kick off the app
+if __name__ == '__main__':
+    sys.exit(main())
